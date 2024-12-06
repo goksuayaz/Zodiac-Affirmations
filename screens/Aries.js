99edcc7a16707a867b294065f984103d0c02dae9
Göksu Ayaz <goksuayaz79@gmail.com>
2024-12-06 15:29:55 +0300

    aries screen edits

--- screens/Aries.js
+++ screens/Aries.js
@@ -1,43 +1,187 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 const Aries = () => {
+  const [activeTab, setActiveTab] = useState('Work&Money');
+
+  const content = {
+    "Work&Money": [
+      "I am a natural leader, and opportunities flow effortlessly to me.",
+
+      "I attract financial abundance with my boldness and determination.",
+
+      "My ambition and energy open doors to endless career opportunities.",
+
+      "I am a magnet for exciting projects and rewarding opportunities.",
+
+
+      "My confidence and passion attract wealth and prosperity into my life."
+
+
+    ],
+    "Love": [
+      "I radiate confidence and passion, attracting deep and meaningful love into my life.",
+
+      "My bold heart leads me to fulfilling and exciting relationships.",
+
+      "I deserve a love that matches my energy, enthusiasm, and authenticity.",
+
+      "I am open to giving and receiving love freely and fearlessly.",
+
+      "My courage and honesty create a strong and passionate connection with my partner."
+
+
+
+
+    ],
+    "Health": [
+      "My vibrant energy fuels my body, keeping me strong and healthy.",
+
+      "I listen to my body’s needs and make choices that support my well-being.",
+
+      "My active and adventurous spirit keeps me fit and full of life.",
+
+      "I embrace balance and nurture my mind, body, and soul daily.",
+
+      "With determination and focus, I build a lifestyle that promotes lasting health and vitality."
+
+
+    ],
+    "Spirituality": [
+      "My fiery spirit connects me deeply to the universe’s boundless energy.",
+
+      "I trust my intuition to guide me on my spiritual journey with courage and clarity.",
+
+      "My inner strength fuels my growth and aligns me with my highest purpose.",
+
+      "I embrace new spiritual paths with an open and fearless heart.",
+
+      "Through self-reflection and determination, I awaken to my true potential and divine connection."
+
+    ]
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Aries Affirmations</Text>
-      <Text style={styles.affirmation}>I nourish my courage with strength every moment and step fearlessly into new beginnings.</Text>
-      <Text style={styles.affirmation}>I inspire and guide others with my leadership skills.</Text>
-      <Text style={styles.affirmation}>My energy and passion provide me with unlimited power to achieve my goals.</Text>
-      <Text style={styles.affirmation}>With determination and perseverance, I can overcome any challenge.</Text>
-      <Text style={styles.affirmation}>I create new opportunities every day to improve myself.</Text>
-      <Text style={styles.affirmation}>With my natural bravery, I turn risks into opportunities.</Text>
-      <Text style={styles.affirmation}>I take control of my life with love and wisdom.</Text>
-      <Text style={styles.affirmation}>Through honest and open communication, I build trust around me.</Text>
-      <Text style={styles.affirmation}>With my creativity and confidence, I make a unique contribution to every project.</Text>
-      <Text style={styles.affirmation}>Obstacles make me stronger and more resilient.</Text>
+
+      <View style={styles.imagearea}>
+        <Image style={styles.imagearies} source={require("../assets/visuals/aries.jpg")} />
+      </View>
+
+      <View style={styles.titlearea}>
+        <Text style={styles.title}>Aries Affirmations</Text>
+      </View>
+
+      <View style={styles.buttonsarea}>
+        <View style={styles.buttons}>
+          {Object.keys(content).map((tab) => (
+            <TouchableOpacity
+              key={tab}
+              style={[
+                styles.button,
+                activeTab === tab && styles.activeButton,
+              ]}
+              onPress={() => setActiveTab(tab)}
+            >
+              <Text style={styles.categories}>{tab}</Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+
+      <View style={styles.contentArea}>
+        <Text style={styles.affirmation}>{content[activeTab]}</Text>
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: "#dc2f02",
+  },
+
+  imagearea: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    width: 320,
+    height: 320,
+    backgroundColor: "#fff",
+    borderRadius: 16,
+    marginLeft: 50,
+  },
+
+
+  imagearies: {
+    width: 300,
+    height: 300,
+    borderRadius: 8,
+  },
+
+
+  titlearea: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
   },
 
   title: {
     textAlign: 'center',
-    marginTop: 50,
-    fontSize: 18,
+    marginTop: 12,
+    fontSize: 25,
     fontWeight: 'bold',
   },
 
-  affirmation: {
-    marginTop: 20,
-    fontSize: 16,
-    lineHeight: 24,
-    marginLeft: 16
 
-  }
-})
+  buttonsarea: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 20,
+    marginLeft: 15,
+    marginRight: 15,
+  },
+
+  button: {
+    width: 85,
+    height: 25,
+    backgroundColor: "#fff",
+    marginHorizontal: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+  },
+
+
+  activeButton: {
+    backgroundColor: "#fdc100",
+  },
+
+
+  categories: {
+    fontSize: 12,
+    textAlign: "center",
+  },
+
+  contentArea: {
+    marginTop: 30,
+    paddingHorizontal: 16,
+  },
+
+
+  affirmation: {
+    fontSize: 16,
+    lineHeight: 22,
+    textAlign: "center",
+    color: "#fff",
+  },
+});
 
 export default Aries;
--- screens/Taurus.js
+++ screens/Taurus.js
@@ -1,40 +1,157 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native';
 
 const Taurus = () => {
+
+  const [activeTab, setActiveTab] = useState("Work&Money");
+
+  const content = {
+    "Work&Money": [
+
+
+
+    ],
+
+    "Love": [
+
+
+    ],
+
+    "Health": [
+
+    ],
+
+    "Sprituality": [
+
+
+    ]
+  }
+
+
   return (
+
     <View style={styles.container}>
-      <Text style={styles.title}>Taurus Affirmations</Text>
-      <Text style={styles.affirmation}>I build security and peace in my life with love.</Text>
-      <Text style={styles.affirmation}>My patience and determination help me achieve all my goals.</Text>
-      <Text style={styles.affirmation}>My strength and resilience help me overcome every challenge I face.</Text>
-      <Text style={styles.affirmation}>The value I give myself enriches and beautifies my life.</Text>
-      <Text style={styles.affirmation}>I feel the peace and energy of nature within me at all times.</Text>
-      <Text style={styles.affirmation}>WWith my loyal and loving heart, I bring trust and happiness to those around me.</Text>
-      <Text style={styles.affirmation}>Abundance and prosperity are my natural living spaces.</Text>
-      <Text style={styles.affirmation}>My artistic spirit and creativity add grace to everything I do.</Text>
-      <Text style={styles.affirmation}>I enjoy life while building a solid future.</Text>
-      <Text style={styles.affirmation}>The bonds I build with love make my life meaningful and valuable.</Text>
+
+      <View style={styles.imagearea}>
+        <Image style={styles.imagetaurus} source={require("../assets/visuals/tauruss.jpg")} />
+      </View>
+
+
+
+      <View style={styles.titlearea}>
+        <Text style={styles.title}>Taurus Affirmations</Text>
+      </View>
+
+
+      <View style={styles.buttonsarea}>
+        <View style={styles.buttons}>
+          {Object.keys(content).map((tab) => (
+            <TouchableOpacity
+              key={tab}
+              style={[
+                styles.button, activeTab === tab && styles.activeButton,
+
+              ]} onPress={() => setActiveTab(tab)}>
+              <Text style={styles.categories}> {tab} </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+
+      <View style={styles.contentArea}>
+        <Text style={styles.affirmation}>{content[activeTab]}</Text>
+      </View>
+
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    backgroundColor: "#008000"
   },
+
+  imagearea: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 100,
+    width: 320,
+    height: 320,
+    backgroundColor: "#fff",
+    borderRadius: 16,
+    marginLeft: 50,
+
+  },
+
+  imagetaurus: {
+    width: 300,
+    height: 300,
+    borderRadius: 8,
+  },
+
+  titlearea: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 20,
+  },
+
+
   title: {
     textAlign: 'center',
-    marginTop: 50,
-    fontSize: 18,
+    marginTop: 12,
+    fontSize: 25,
     fontWeight: 'bold',
   },
-  affirmation: {
-    marginTop: 20,
-    fontSize: 16,
-    lineHeight: 24,
-    marginLeft: 16
 
+  buttonsarea: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+
+  buttons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    marginTop: 20,
+    marginLeft: 15,
+    marginRight: 15,
+  },
+
+
+  button: {
+    width: 85,
+    height: 25,
+    backgroundColor: "#fff",
+    marginHorizontal: 5,
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 5,
+  },
+
+
+  activeButton: {
+    backgroundColor: "#fdc100",
+  },
+
+  categories: {
+    fontSize: 12,
+    textAlign: "center",
+  },
+
+  contentArea: {
+    marginTop: 30,
+    paddingHorizontal: 16,
+
+  },
+
+
+  affirmation: {
+    fontSize: 16,
+    lineHeight: 22,
+    textAlign: "center",
+    color: "#fff",
   }
 })
 
